2d056645c2a3883162c7273af180d9a6a24675e5
dhuseljic <d.huseljic17@gmail.com>
2020-05-07 11:18:38 +0200

    working version

--- vae.py
+++ vae.py
@@ -11,61 +11,50 @@ import torch
 import torch.nn as nn
 
 import torchvision
-from utils.datasets import load_mnist
-from utils.vae import Unflatten
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-
-def train_vae():
-    train_ds, val_ds, test_ds = load_mnist()
+def main():
+    transform = torchvision.transforms.ToTensor()
+    train_ds = torchvision.datasets.MNIST('~/.datasets', download=True, transform=transform)
     train_loader = torch.utils.data.DataLoader(train_ds, batch_size=32)
 
-    encoder = Encoder()
-    decoder = nn.Sequential(
-        nn.Linear(392, 784), nn.ReLU(),
-        nn.Linear(784, 784), nn.Sigmoid(), Unflatten(1, 28, 28)
-    )
+    vae = VariationalAutoencoder()
+    optimizer = torch.optim.Adam(vae.parameters())
 
-    optimizer_encoder = torch.optim.Adam(encoder.parameters())
-    optimizer_decoder = torch.optim.Adam(decoder.parameters())
-
-    encoder.to(device)
-    decoder.to(device)
-
+    vae.to(device)
     fixed_noise = torch.randn(32, 392, device=device)
 
     # Algorithm from [1]
     for X_batch, _ in tqdm.tqdm(train_loader):
         X_batch = X_batch.to(device)
 
-        mean, logvar, lvals = encoder(X_batch)
-        eps, z = encoder.reparam(mean, logvar, lvals)
+        mean, logvar, lvals = vae.encode(X_batch)
+        eps, z = vae.reparam(mean, logvar, lvals)
 
-        log_qz = -torch.sum(
-            .5*(eps**2 + math.log(2 * math.pi) + torch.sqrt(torch.exp(logvar))), -1)
+        log_qz = -torch.sum(.5*(eps**2 + math.log(2 * math.pi) + logvar), -1)
         log_pz = -torch.sum(.5*(z**2 + math.log(2*math.pi)), -1)
 
-        p = decoder(z)
+        p = vae.decoder(z)
         log_px = torch.sum(X_batch * torch.log(p) + (1 - X_batch) * torch.log(1 - p), dim=(1, 2, 3))
 
         loss = -(torch.mean(log_px) + torch.mean(log_pz) - torch.mean(log_qz))
+        optimizer.zero_grad()
         loss.backward()
-        optimizer_decoder.step()
-        optimizer_decoder.zero_grad()
-        optimizer_encoder.step()
-        optimizer_encoder.zero_grad()
+        optimizer.step()
 
     with torch.no_grad():
-        img = torchvision.utils.make_grid(decoder(fixed_noise))
+        img = torchvision.utils.make_grid(vae.decoder(fixed_noise))
         plt.imshow(img.cpu().numpy().transpose((1, 2, 0)))
         plt.show()
 
 
-class Encoder(nn.Module):
+class VariationalAutoencoder(nn.Module):
     def __init__(self):
         super().__init__()
         self.n_latent = 392
+
+        # Encoder Example
         self.flatten = nn.Flatten()
         self.l1 = nn.Linear(784, 784)
         self.l2_mean = nn.Linear(784, self.n_latent)  # mean of q(z|x)
@@ -79,7 +68,12 @@ class Encoder(nn.Module):
 
         self.l2_Lvals = nn.Linear(784, len(self.tri_idx_col))  # cholesky of cov
 
-    def forward(self, x):
+        self.decoder = nn.Sequential(
+            nn.Linear(392, 784), nn.ReLU(),
+            nn.Linear(784, 784), nn.Sigmoid(), Unflatten(1, 28, 28)
+        )
+
+    def encode(self, x):
         x = self.flatten(x)
         x = self.l1(x)
 
@@ -106,6 +100,16 @@ class Encoder(nn.Module):
 
         return eps, sample
 
+class Unflatten(nn.Module):
+    def __init__(self, channel, height, width):
+        super().__init__()
+        self.channel = channel
+        self.height = height
+        self.width = width
+
+    def forward(self, input):
+        return input.view(input.size(0), self.channel, self.height, self.width)
+
 
 if __name__ == "__main__":
-    train_vae()
+    main()
